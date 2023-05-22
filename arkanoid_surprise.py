36868150dbc50df89d2a8eccbe3fd63c7d001393
ciamew <113471943+ciamew@users.noreply.github.com>
2023-05-22 20:40:36 +0200

    Update and rename prerabka.py to arkanoid_surprise.py

--- arkanoid_surprise.py
+++ arkanoid_surprise.py
@@ -27,11 +27,41 @@ bar = canvas.create_rectangle(0,h,w,h,outline="red")
 for q in range(0,66):  #vytvori spodne trojuholniky
     canvas.create_polygon(10*q,450,5+(10*q),440,10*(q+1),450,outline="red",fill="red")
 
-canvas.create_text(67,350,text="BALL SPEED",fill="darkblue",font="Arial 7")
-speed_up_rect = canvas.create_rectangle(40,370,64,394,outline="white")
+ball_speed_text = canvas.create_text(67,350,text="BALL SPEED")
+#speed_up_rect = canvas.create_rectangle(40,370,64,394,outline="white")
 speed_up_plus = canvas.create_polygon(48,370,56,370,56,378,64,378,64,386,56,386,56,386,56,394,48,394,48,386,40,386,40,378,48,378,fill="darkblue",outline="darkblue")
-speed_down_rect = canvas.create_rectangle(70,370,94,394,outline="white")
+#speed_down_rect = canvas.create_rectangle(70,370,94,394,outline="white")
 speed_down_minus = canvas.create_rectangle(70,378,94,386,fill="darkblue",outline="darkblue")
+
+
+change_bg_color_text = canvas.create_text(520,255,text="CHANGE BACKGROUND COLOR --->\n(by clicking on the given number)")
+bg_white = canvas.create_rectangle(620,240,640,260,fill="white")
+bg_white_text = canvas.create_text(630,250,text="1")
+bg_beige = canvas.create_rectangle(620,270,640,290,fill="beige")
+bg_beige_text = canvas.create_text(630,280,text="2")
+bg_yellow = canvas.create_rectangle(620,300,640,320,fill="yellow")
+bg_yellow_text = canvas.create_text(630,310,text="3")
+bg_orange = canvas.create_rectangle(620,330,640,350,fill="orange")
+bg_orange_text = canvas.create_text(630,340,text="4")
+bg_darkkhaki = canvas.create_rectangle(620,360,640,380,fill="darkkhaki")
+bg_darkkhaki_text = canvas.create_text(630,370,text="5")
+
+def change_bg(e):
+    # print("you pressed"+ e.char)
+    if e.char == "1":
+        # print("ide")
+        canvas.configure(bg="white")
+    if e.char == "2":
+        canvas.configure(bg="beige")
+    if e.char == "3":
+        canvas.configure(bg="yellow")
+    if e.char == "4":
+        canvas.configure(bg="orange")
+    if e.char == "5":
+        canvas.configure(bg="darkkhaki")
+
+for bg in range(1,6):
+    win.bind(str(bg), change_bg)
 
 bricks_w = 65
 bricks_h = 20
@@ -44,8 +74,11 @@ bricks = []
 def makebricks():
     for y in range(brick_count_y):
         for x in range(brick_count_x):
-            bricks.append(canvas.create_rectangle(x*bricks_w, y*bricks_h, x*bricks_w + bricks_w, y*bricks_h + bricks_h, fill = bricks_colors[y%brick_count_y],width=5,outline="white"))
+            bricks_feature = canvas.create_rectangle(x * bricks_w, y * bricks_h, x * bricks_w + bricks_w,y * bricks_h + bricks_h, fill=bricks_colors[y % brick_count_y],width=5, outline="white")
+            # bricks.append(canvas.create_rectangle(x*bricks_w, y*bricks_h, x*bricks_w + bricks_w, y*bricks_h + bricks_h, fill = bricks_colors[y%brick_count_y],width=5,outline="white"))
+            bricks.append(bricks_feature)
 makebricks()
+
 
 
 def destroybrick():
@@ -79,7 +112,8 @@ def ball_move():
         movement[0] *= (-1)
     if canvas.coords(ball)[3] > (h-10):
         canvas.delete("all")
-        canvas.create_text(w/2, h/2, text="                     YOU LOST \n TRY AGAIN BY CLICKING ON XXX", fill="red", font="Arial 9")
+        canvas.create_text(w/2, h/2, text="                                    YOU LOST \n TRY AGAIN BY RESTARTING THE WHOLE GAME", fill="red", font="Arial 9")
+        starter()
     if ball in overlap:
         movement = bounce(canvas.coords(ball), paddle_pos)
     # canvas.after(100, ball_move)
@@ -98,7 +132,8 @@ def starter(e):
         x = e.x
         y = e.y
         ball_move()
-        canvas.delete(text_start)
+        canvas.delete(text_start,speed_up_plus,ball_speed_text,speed_down_minus,change_bg_color_text,speed)
+        #canvas.delete("all")
 
 def mover(e):
     global x
@@ -107,30 +142,21 @@ def mover(e):
         canvas.move(paddle, mouse, 0)
         x = e.x
 
-
-# def moverarrow(j):
-#     global x
-#     if x != 0:
-#         arrow = e.x - x
-#         canvas.move(paddle, arrow, 0)
-#         x = e.x
-
+speed = canvas.create_text(w//2,420,text="(to make the ball faster,you have to click on the paddle several times) \n                                               idk y",font="Arial 7")
 # def speed():
 #     canvas.after(50,ball_move)
 
-def checkkey(s):
-    print("Stlacila som")
-    print(s.char)
+# def checkkey(s):
+#     print("Stlacila som")
+#     print(s.char)
 
 canvas.bind("<Button-1>", starter)
 canvas.bind("<B1-Motion>", mover)
 # canvas.bind("<Button-1>", speedup)
-
-win.bind("<Key>",checkkey)
+# win.bind("<Key>",checkkey)
 # win.bind("<Up>",speed)
-win.bind("<Down>",checkkey)
-win.bind("<Right>",checkkey)
-win.bind("<Left>",checkkey)
-
+# win.bind("<Down>",checkkey)
+# win.bind("<Right>",checkkey)
+# win.bind("<Left>",checkkey)
 
 win.mainloop()
